22d21a48386a7e3740a79c601dc9137f4b15f027
Tiril Fjell <tiril.fjell@gmail.com>
2021-09-01 16:20:43 +0200

    new fetch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="home">
     <h3>Hallo test pokemon</h3>
+    {{pokemons}}
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import { reactive, toRefs } from "vue";
 
 export default {
   name: "Home",
-  components: {}
+  setup() {
+    const state = reactive({
+      pokemons: [],
+      urlIdLookup: {}
+    });
+
+    fetch("https://pokeapi.co/api/v2/pokemon?offset=0")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        state.pokemons = data.result;
+        state.urlIdLookup = data.result.reduce(
+          (acc, cur, idx) => (acc = { ...acc, [cur.name]: idx + 1 }),
+          {}
+        );
+      });
+    return { ...toRefs(state) };
+  }
 };
 </script>
