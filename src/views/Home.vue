6266bfbd1e67c5b8560611e965c6a42c2cb468d6
Tiril Fjell <tiril.fjell@gmail.com>
2021-09-01 15:52:10 +0200

    fist commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    <h3>Hallo test pokemon</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "Home",
+  components: {}
+};
+</script>
